37fabd7b0eda1caf18732c3877a5c7dcd7fb5964
barry <lavidesbarry@gmail.com>
2024-04-13 13:43:37 +0800

    Separating the import of Network and using "rand-std" instead of rand in cargo.toml solves the error of thread_rng not found in rand

--- src/bitcoin_client.rs
+++ src/bitcoin_client.rs
@@ -1,5 +1,10 @@
-use bitcoin::secp256k1::Secp256k1;
+use bitcoin::Network;
+use bitcoin::secp256k1::{rand, Secp256k1, Keypair};
 
 pub fn generate_wallet() {
     let elliptic_curve_fn = Secp256k1::new();
+    let network = Network::Bitcoin;
+    
+    let (secret_key, public_key) = elliptic_curve_fn.generate_keypair(&mut rand::thread_rng());
+    let key_pair = Keypair::from_secret_key(&elliptic_curve_fn, &secret_key);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod bitcoin_client;
+
 fn main() {
     println!("Hello, world!");
 }
