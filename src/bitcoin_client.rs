5d0c9b236eebda5520a4984339ee3435469b2da0
barry <lavidesbarry@gmail.com>
2024-04-13 14:17:23 +0800

    changed the network to testnet

--- src/bitcoin_client.rs
+++ src/bitcoin_client.rs
@@ -3,7 +3,7 @@ use bitcoin::secp256k1::{rand, Secp256k1, Keypair};
 
 pub fn generate_wallet() {
     let elliptic_curve_fn = Secp256k1::new();
-    let network = Network::Bitcoin;
+    let network = Network::Testnet;
     
     let (secret_key, public_key) = elliptic_curve_fn.generate_keypair(&mut rand::thread_rng());
     let keypair = Keypair::from_secret_key(&elliptic_curve_fn, &secret_key);
