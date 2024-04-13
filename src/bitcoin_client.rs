b5bbda672d6c75777230b6c8bad785a0cda155fc
barry <lavidesbarry@gmail.com>
2024-04-13 14:09:22 +0800

    p2wpkh and p2tr address print are working properly

--- src/bitcoin_client.rs
+++ src/bitcoin_client.rs
@@ -1,4 +1,4 @@
-use bitcoin::Network;
+use bitcoin::{Address, Network, XOnlyPublicKey};
 use bitcoin::secp256k1::{rand, Secp256k1, Keypair};
 
 pub fn generate_wallet() {
@@ -6,5 +6,11 @@ pub fn generate_wallet() {
     let network = Network::Bitcoin;
     
     let (secret_key, public_key) = elliptic_curve_fn.generate_keypair(&mut rand::thread_rng());
-    let key_pair = Keypair::from_secret_key(&elliptic_curve_fn, &secret_key);
+    let keypair = Keypair::from_secret_key(&elliptic_curve_fn, &secret_key);
+    let (x_only_public_key, parity) = XOnlyPublicKey::from_keypair(&keypair);
+    let tap_address = Address::p2tr(&elliptic_curve_fn, x_only_public_key, None, network);
+    let p2wpkh_address = Address::p2wpkh(&bitcoin::CompressedPublicKey(public_key), network);
+
+    println!("pay to witness public key hash {}", p2wpkh_address.to_qr_uri().to_ascii_lowercase());
+    println!("pay to taproot address {}", tap_address.to_qr_uri().to_ascii_lowercase())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod bitcoin_client;
 
 fn main() {
+    bitcoin_client::generate_wallet();
     println!("Hello, world!");
 }
